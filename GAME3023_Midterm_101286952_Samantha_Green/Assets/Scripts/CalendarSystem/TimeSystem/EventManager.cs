dc1f5f38f2416abd849385e64925a25212a2599d
Samantha Green <115169809+elentiyafireheart@users.noreply.github.com>
2024-11-15 23:31:08 -0500

    Final Push

--- GAME3023_Midterm_101286952_Samantha_Green/Assets/Scripts/CalendarSystem/TimeSystem/EventManager.cs
+++ GAME3023_Midterm_101286952_Samantha_Green/Assets/Scripts/CalendarSystem/TimeSystem/EventManager.cs
@@ -5,49 +5,5 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-    //[Header("Event List")] public List<EventObject> eventObjects;
-    //private CalendarManager calendarManager;
-
-    //private void Awake()
-    //{
-    //    calendarManager = FindObjectOfType<CalendarManager>();
-    //}
-
-    //public void UpdateEvents()
-    //{
-    //    foreach (var eventObject in eventObjects)
-    //    {
-    //        if (eventObject != null)
-    //        {
-    //            eventObject.TriggerEvent(calendarManager);
-    //        }
-    //    }
-    //}
-
-    //public List<string> GetTriggeredEventNames(int currentDay, string currentSeason)
-    //{
-    //    List<string> triggeredEventNames = new List<string>();
-
-    //    foreach (var eventObject in eventObjects)
-    //    {
-    //        if (eventObject != null && eventObject.IsEventTriggered(currentDay, currentSeason))
-    //        {
-    //            triggeredEventNames.Add(eventObject.eventName);
-    //        }
-    //    }
-
-    //    return triggeredEventNames;
-    //}
-
-    //public void HighlightEventDays(int currentDay, string currentSeason)
-    //{
-    //    foreach (var eventObject in eventObjects)
-    //    {
-    //        if (eventObject != null)
-    //        {
-    //            // Check if this event is triggered on the current day
-    //            eventObject.HighlightEventDay(currentDay, currentSeason, calendarManager.GetDayText(currentDay));
-    //        }
-    //    }
-    //}
+ 
 }

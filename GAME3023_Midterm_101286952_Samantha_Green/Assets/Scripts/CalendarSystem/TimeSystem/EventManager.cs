1ce573e2aa6769d2a3a069addefe14307608accb
Samantha Green <115169809+elentiyafireheart@users.noreply.github.com>
2024-11-15 22:45:16 -0500

    Added Scripts to EventPrefabs

--- GAME3023_Midterm_101286952_Samantha_Green/Assets/Scripts/CalendarSystem/TimeSystem/EventManager.cs
+++ GAME3023_Midterm_101286952_Samantha_Green/Assets/Scripts/CalendarSystem/TimeSystem/EventManager.cs
@@ -5,48 +5,49 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-    [Header("Event List")] public List<EventObject> eventObjects;
-    private CalendarManager calendarManager;
+    //[Header("Event List")] public List<EventObject> eventObjects;
+    //private CalendarManager calendarManager;
 
-    private void Awake()
-    {
-        calendarManager = FindObjectOfType<CalendarManager>();
-    }
+    //private void Awake()
+    //{
+    //    calendarManager = FindObjectOfType<CalendarManager>();
+    //}
 
-    public void UpdateEvents()
-    {
-        foreach (var eventObject in eventObjects)
-        {
-            if (eventObject != null)
-            {
-                eventObject.TriggerEvent(calendarManager);
-            }
-        }
-    }
+    //public void UpdateEvents()
+    //{
+    //    foreach (var eventObject in eventObjects)
+    //    {
+    //        if (eventObject != null)
+    //        {
+    //            eventObject.TriggerEvent(calendarManager);
+    //        }
+    //    }
+    //}
 
-    public List<string> GetTriggeredEventNames(int currentDay, string currentSeason)
-    {
-        List<string> triggeredEventNames = new List<string>();
+    //public List<string> GetTriggeredEventNames(int currentDay, string currentSeason)
+    //{
+    //    List<string> triggeredEventNames = new List<string>();
 
-        foreach (var eventObject in eventObjects)
-        {
-            if (eventObject != null && eventObject.IsEventTriggered(currentDay, currentSeason))
-            {
-                triggeredEventNames.Add(eventObject.eventName);
-            }
-        }
+    //    foreach (var eventObject in eventObjects)
+    //    {
+    //        if (eventObject != null && eventObject.IsEventTriggered(currentDay, currentSeason))
+    //        {
+    //            triggeredEventNames.Add(eventObject.eventName);
+    //        }
+    //    }
 
-        return triggeredEventNames;
-    }
+    //    return triggeredEventNames;
+    //}
 
-    public void HighlightEventDays(int currentDay, string currentSeason)
-    {
-        foreach (var eventObject in eventObjects)
-        {
-            if (eventObject != null)
-            {
-                eventObject.HighlightEventDay(currentDay, currentSeason, calendarManager.GetDayText(currentDay));
-            }
-        }
-    }
+    //public void HighlightEventDays(int currentDay, string currentSeason)
+    //{
+    //    foreach (var eventObject in eventObjects)
+    //    {
+    //        if (eventObject != null)
+    //        {
+    //            // Check if this event is triggered on the current day
+    //            eventObject.HighlightEventDay(currentDay, currentSeason, calendarManager.GetDayText(currentDay));
+    //        }
+    //    }
+    //}
 }
